1e93cb5bbdfe60c388528febb11dc707a903f2ac
Yubraj Pokharel <yubraj.pokharel555@gmail.com>
2019-07-18 01:06:18 -0500

    sample greet server

---
+++ src/main/java/com/yubraj/gRPC/client/ClientServer.java
@@ -0,0 +1,35 @@
+package com.yubraj.gRPC.client;
+
+import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.*;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class ClientServer {
+
+    public static void main(String[] args) {
+        System.out.println("Starting client server");
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50052)
+                .usePlaintext().build();
+        System.out.println("Creating stubs");
+
+        GreetServiceGrpc.GreetServiceBlockingStub syncGreetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        Greeting greeting = Greeting.newBuilder().
+                setFirstName("yubraj")
+                .setLastName("pokharel")
+                .build();
+
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        GreetResponse greetResponse = syncGreetClient.greet(greetRequest);
+
+        System.out.println(greetResponse.getResult());
+
+        System.out.println("Shutting dowm channel");
+        channel.shutdown();
+    }
+
+}
---
+++ src/main/java/com/yubraj/gRPC/server/GreetServiceImpl.java
@@ -0,0 +1,21 @@
+package com.yubraj.gRPC.server;
+
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
+import io.grpc.stub.StreamObserver;
+
+public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+        Greeting greeting = request.getGreeting();
+        String firstName = greeting.getFirstName();
+        String lastName = greeting.getLastName();
+        String result = "Hello " + firstName + " " + lastName;
+        GreetResponse response = GreetResponse.newBuilder().setResult(result).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
